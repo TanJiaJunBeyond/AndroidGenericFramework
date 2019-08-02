a1349270ad469d4c2021be06590d516582ddb720
Kevin Tan <ktan@xogrp.com>
2019-08-03 01:51:40 +0800

    使用MVVM搭建框架。

--- app/src/main/java/com/tanjiajun/dadarecycle/DaDaRecycleApplication.kt
+++ app/src/main/java/com/tanjiajun/dadarecycle/DaDaRecycleApplication.kt
@@ -1,8 +1,24 @@
 package com.tanjiajun.dadarecycle
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.multidex.MultiDexApplication
+import org.litepal.LitePal
 
 /**
  * Created by TanJiaJun on 2019-07-28.
  */
-class DaDaRecycleApplication : MultiDexApplication()
+class DaDaRecycleApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        LitePal.initialize(this)
+        context = this
+    }
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+}
--- app/src/main/java/com/tanjiajun/dadarecycle/user/ui/user/fragment/LoginFragment.kt
+++ app/src/main/java/com/tanjiajun/dadarecycle/user/ui/user/fragment/LoginFragment.kt
@@ -9,22 +9,29 @@ import com.tanjiajun.dadarecycle.BaseFragment
 import com.tanjiajun.dadarecycle.FRAGMENT_TAG_LOGIN
 import com.tanjiajun.dadarecycle.R
 import com.tanjiajun.dadarecycle.databinding.FragmentLoginBinding
+import com.tanjiajun.dadarecycle.user.ui.user.viewModel.LoginViewModel
 
 /**
  * Created by TanJiaJun on 2019-07-29.
  */
-class LoginFragment : BaseFragment() {
+class LoginFragment
+    : BaseFragment(),
+        LoginViewModel.Handlers {
 
     override fun getLayoutResource(): Int = R.layout.fragment_login
 
     override fun getTransactionTag(): String = FRAGMENT_TAG_LOGIN
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        DataBindingUtil.inflate<FragmentLoginBinding>(inflater, getLayoutResource(), container, false).root
+            DataBindingUtil.inflate<FragmentLoginBinding>(inflater, getLayoutResource(), container, false).root
+
+    override fun onLoginClick(view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     companion object {
         fun newInstance() =
-            LoginFragment()
+                LoginFragment()
     }
 
 }
