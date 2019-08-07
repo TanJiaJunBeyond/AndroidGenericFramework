45687a18d43a5f97a1c37afecf1ae5d3e3f1be4a
TanJiaJun <1120571286@qq.com>
2019-08-08 04:19:27 +0800

    Update DaDaRecycleApplication.kt

--- app/src/main/java/com/tanjiajun/dadarecycle/DaDaRecycleApplication.kt
+++ app/src/main/java/com/tanjiajun/dadarecycle/DaDaRecycleApplication.kt
@@ -6,7 +6,6 @@ import androidx.multidex.MultiDexApplication
 import com.tanjiajun.dadarecycle.data.dao.UserDao
 import com.tanjiajun.dadarecycle.data.network.UserNetwork
 import com.tanjiajun.dadarecycle.data.repository.UserRepository
-import org.litepal.LitePal
 
 /**
  * Created by TanJiaJun on 2019-07-28.
@@ -18,7 +17,6 @@ class DaDaRecycleApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        LitePal.initialize(this)
         context = this
     }
 
