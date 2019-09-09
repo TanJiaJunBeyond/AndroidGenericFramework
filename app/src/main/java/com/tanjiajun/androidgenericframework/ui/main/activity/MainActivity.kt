162686310eed6d963b62473ee97c2909a70d2fde
TanJiaJun <1120571286@qq.com>
2019-09-10 02:38:47 +0800

    订单页面添加和删除功能。

--- app/src/main/java/com/tanjiajun/androidgenericframework/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/tanjiajun/androidgenericframework/ui/main/activity/MainActivity.kt
@@ -6,8 +6,10 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.tanjiajun.androidgenericframework.EXTRA_LOGOUT
 import com.tanjiajun.androidgenericframework.R
 import com.tanjiajun.androidgenericframework.databinding.ActivityMainBinding
@@ -26,6 +28,21 @@ class MainActivity
     : BaseActivity(), MainViewModel.Handlers {
 
     private val binding by lazy { DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main) }
+    private val viewModel by lazy { ViewModelProviders.of(this)[MainViewModel::class.java] }
+
+    private val tlOrder: TabLayout
+        get() = binding.tlOrder
+
+    private val vpOrder: ViewPager2
+        get() = binding.vpOrder
+
+    private val list = mutableListOf<OrderFragment>().apply {
+        for (i in 0 until ORDER_ITEM_COUNT) {
+            add(OrderFragment.newInstance(i))
+        }
+    }
+
+    private val adapter by lazy { OrderFragmentStateAdapter(this@MainActivity, list) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,29 +50,25 @@ class MainActivity
             lifecycleOwner = this@MainActivity
             handlers = this@MainActivity
         }
-    }
 
-    override fun onResume() {
-        super.onResume()
         initUI()
     }
 
-    private fun initUI() =
-            binding.run {
-                for (i in 0 until ORDER_ITEM_COUNT) {
-                    tlOrder.addTab(tlOrder.newTab().setText("订单$i"))
-                }
+    private fun initUI() {
+        for (i in 0 until ORDER_ITEM_COUNT) {
+            tlOrder.addTab(tlOrder.newTab().setText("订单$i"))
+        }
 
-                tlOrder.addOnTabSelectedListener(registerOnTabSelectedListener {
-                    onTabSelected { vpOrder.currentItem = it?.position ?: 0 }
-                })
+        tlOrder.addOnTabSelectedListener(registerOnTabSelectedListener {
+            onTabSelected { vpOrder.currentItem = it?.position ?: 0 }
+        })
 
-                vpOrder.adapter = OrderFragmentStateAdaper(this@MainActivity)
-                vpOrder.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                    override fun onPageSelected(position: Int) =
-                            tlOrder.setScrollPosition(position, 0f, true)
-                })
-            }
+        vpOrder.adapter = adapter
+        vpOrder.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) =
+                    tlOrder.setScrollPosition(position, 0f, true)
+        })
+    }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
@@ -71,15 +84,32 @@ class MainActivity
     override fun onPersonalCenterClick(view: View) =
             startActivity<PersonalCenterActivity>()
 
+    override fun onStateClick(view: View) {
+        if (viewModel.added.value == true) {
+            tlOrder.removeTabAt(ORDER_ITEM_COUNT)
+            list.removeAt(ORDER_ITEM_COUNT)
+            adapter.notifyItemRemoved(ORDER_ITEM_COUNT)
+            vpOrder.currentItem = 0
+        } else {
+            tlOrder.addTab(tlOrder.newTab().setText("订单$ORDER_ITEM_COUNT"))
+            list.add(OrderFragment.newInstance(ORDER_ITEM_COUNT))
+            adapter.notifyItemInserted(ORDER_ITEM_COUNT)
+            vpOrder.currentItem = ORDER_ITEM_COUNT
+        }
+
+        viewModel.changeState()
+    }
+
 }
 
-class OrderFragmentStateAdaper(fragmentActivity: FragmentActivity)
+class OrderFragmentStateAdapter(fragmentActivity: FragmentActivity,
+                                private val list: List<OrderFragment>)
     : FragmentStateAdapter(fragmentActivity) {
 
     override fun createFragment(position: Int): Fragment =
-            OrderFragment.newInstance(position)
+            list[position]
 
-    override fun getItemCount(): Int = ORDER_ITEM_COUNT
+    override fun getItemCount(): Int = list.size
 
 }
 
--- app/src/main/java/com/tanjiajun/androidgenericframework/ui/main/viewModel/MainViewModel.kt
+++ app/src/main/java/com/tanjiajun/androidgenericframework/ui/main/viewModel/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.tanjiajun.androidgenericframework.ui.main.viewModel
 
 import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.tanjiajun.androidgenericframework.ui.BaseViewModel
 
 /**
@@ -8,9 +10,18 @@ import com.tanjiajun.androidgenericframework.ui.BaseViewModel
  */
 class MainViewModel : BaseViewModel() {
 
+    private val _added = MutableLiveData<Boolean>()
+    val added: LiveData<Boolean> = _added
+
+    fun changeState() {
+        _added.value = !(_added.value ?: false)
+    }
+
     interface Handlers {
 
         fun onPersonalCenterClick(view: View)
+
+        fun onStateClick(view: View)
 
     }
 
