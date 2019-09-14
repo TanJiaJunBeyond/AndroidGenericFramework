6aebf0613ecdc227c59ef6f730ed15b996ff9288
TanJiaJun <1120571286@qq.com>
2019-09-14 16:21:43 +0800

    优化代码。

--- app/src/main/java/com/tanjiajun/androidgenericframework/ui/order/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/tanjiajun/androidgenericframework/ui/order/viewmodel/OrderViewModel.kt
@@ -14,15 +14,18 @@ class OrderViewModel(
     fun getOrderList(position: Int): List<OrderData> =
             mutableListOf<OrderData>()
                     .apply {
-                        for (i in 0 until 50) {
+                        for (i in 0 until 100) {
                             add(OrderData(
                                     id = i.toLong(),
-                                    title = "${repository.getUserInfo()?.userName}的第${position}个订单",
+                                    title = repository.getUserInfo()?.run { "${userName}的第${position}个订单" }
+                                            ?: "",
                                     orderNumber = "订单号：$i",
-                                    address = "地址：${repository.getUserInfo()}的地址",
+                                    address = repository.getUserInfo()?.run { "地址：${userName}的地址" }
+                                            ?: "",
                                     date = "日期：2019年9月7日",
                                     weight = 100,
-                                    noteInformation = "备注信息：${repository.getUserInfo()?.userName}的备注信息",
+                                    noteInformation = repository.getUserInfo()?.run { "备注信息：${userName}的备注信息" }
+                                            ?: "",
                                     firstImageUrl = "https://qa-media-api.xogrp.com/images/3932fa03-2437-4ca2-b413-b88ac261bc0f",
                                     secondImageUrl = "https://qa-media-api.xogrp.com/images/error",
                                     thirdImageUrl = null
