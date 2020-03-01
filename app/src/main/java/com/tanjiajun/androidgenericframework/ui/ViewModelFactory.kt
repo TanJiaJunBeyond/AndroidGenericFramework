63f6f9f6a6a9e4c19e100ae0dfc3b828e9ce965d
谭嘉俊 <1120571286@qq.com>
2020-03-01 20:30:14 +0800

    重构ViewModelFactory代码。

--- app/src/main/java/com/tanjiajun/androidgenericframework/ui/ViewModelFactory.kt
+++ app/src/main/java/com/tanjiajun/androidgenericframework/ui/ViewModelFactory.kt
@@ -9,10 +9,58 @@ import com.tanjiajun.androidgenericframework.ui.main.viewModel.SplashViewModel
 import com.tanjiajun.androidgenericframework.ui.repository.viewmodel.RepositoryViewModel
 import com.tanjiajun.androidgenericframework.ui.user.viewmodel.LoginViewModel
 import com.tanjiajun.androidgenericframework.ui.user.viewmodel.PersonalCenterViewModel
+import dagger.Binds
+import dagger.MapKey
+import dagger.Module
+import javax.inject.Inject
+import javax.inject.Provider
+import kotlin.reflect.KClass
 
 /**
  * Created by TanJiaJun on 2019-08-07.
  */
+class AndroidGenericFrameworkViewModelFactory @Inject constructor(
+        private val creators: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+            creators[modelClass]
+                    ?.let {
+                        creators[modelClass]
+                                ?.let { getViewModel<T>(it) }
+                                ?: throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+                    }
+                    ?: creators.entries
+                            .find { modelClass.isAssignableFrom(it.key) }
+                            ?.value
+                            ?.let { getViewModel<T>(it) }
+                    ?: throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+
+    private fun <T : ViewModel?> getViewModel(provider: Provider<ViewModel>): T =
+            try {
+                @Suppress("UNCHECKED_CAST")
+                provider.get() as T
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+
+}
+
+@Module
+internal abstract class ViewModelBuilder {
+
+    @Binds
+    internal abstract fun bindViewModelFactory(
+            factory: AndroidGenericFrameworkViewModelFactory
+    ): ViewModelProvider.Factory
+
+}
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
+
 @Suppress("UNCHECKED_CAST")
 class ViewModelFactory(
         private val userRepository: UserInfoRepository,
