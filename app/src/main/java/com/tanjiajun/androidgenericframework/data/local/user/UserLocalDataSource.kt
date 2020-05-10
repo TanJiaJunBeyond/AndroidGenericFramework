434dc76e7380fe970bc8967bf0cc5710cc5f33d3
谭嘉俊 <1120571286@qq.com>
2020-05-10 16:30:43 +0800

    优化代码。

--- app/src/main/java/com/tanjiajun/androidgenericframework/data/local/user/UserLocalDataSource.kt
+++ app/src/main/java/com/tanjiajun/androidgenericframework/data/local/user/UserLocalDataSource.kt
@@ -1,6 +1,5 @@
 package com.tanjiajun.androidgenericframework.data.local.user
 
-import android.content.SharedPreferences
 import com.tanjiajun.androidgenericframework.utils.int
 import com.tanjiajun.androidgenericframework.utils.string
 import com.tencent.mmkv.MMKV
@@ -18,12 +17,6 @@ class UserLocalDataSource(
     var password by mmkv.string("password", "")
     var name by mmkv.string("name", "")
     var avatarUrl by mmkv.string("avatar_url", "")
-
-    private fun SharedPreferences.edit(function: SharedPreferences.Editor.() -> Unit) =
-            edit().also {
-                function(it)
-                it.apply()
-            }
 
     fun clearUserInfoCache() =
             with(mmkv) {
