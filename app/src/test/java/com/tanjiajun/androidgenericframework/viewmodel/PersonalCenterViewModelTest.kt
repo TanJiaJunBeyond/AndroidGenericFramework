6b3d37747b9b9eeb0e7c6365b0b70200f2fcc79f
谭嘉俊 <1120571286@qq.com>
2020-05-29 02:08:02 +0800

    优化代码。
    (cherry picked from commit 8cd0a09c44abed5424264c2a6b36e56ab2182b33)

--- app/src/test/java/com/tanjiajun/androidgenericframework/viewmodel/PersonalCenterViewModelTest.kt
+++ app/src/test/java/com/tanjiajun/androidgenericframework/viewmodel/PersonalCenterViewModelTest.kt
@@ -20,7 +20,7 @@ import org.junit.runners.JUnit4
  * Created by TanJiaJun on 2020/5/28.
  */
 @RunWith(JUnit4::class)
-class PersonCenterViewModelTest {
+class PersonalCenterViewModelTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
