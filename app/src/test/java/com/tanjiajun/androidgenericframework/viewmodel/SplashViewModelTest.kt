89e704942e9d0958cef428690eea16cf92269245
谭嘉俊 <1120571286@qq.com>
2020-06-06 16:47:11 +0800

    优化代码。

--- app/src/test/java/com/tanjiajun/androidgenericframework/viewmodel/SplashViewModelTest.kt
+++ app/src/test/java/com/tanjiajun/androidgenericframework/viewmodel/SplashViewModelTest.kt
@@ -1,12 +1,22 @@
 package com.tanjiajun.androidgenericframework.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
 import com.tanjiajun.androidgenericframework.data.repository.UserInfoRepository
 import com.tanjiajun.androidgenericframework.ui.main.viewmodel.SplashViewModel
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import org.junit.Assert.assertEquals
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,22 +37,42 @@ class SplashViewModelTest {
 
     private lateinit var viewModel: SplashViewModel
 
+    @ExperimentalCoroutinesApi
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
+        Dispatchers.setMain(Dispatchers.Unconfined)
         viewModel = SplashViewModel(repository)
     }
 
-    @Test
-    fun isLogin_success() {
-        every { repository.isLogin() } returns true
-        assertEquals(true, viewModel.isLogin())
+    @ExperimentalCoroutinesApi
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
-    fun isLogin_failure() {
-        every { repository.isLogin() } returns false
-        assertEquals(false, viewModel.isLogin())
+    fun navigateToPage_success() {
+        runBlocking {
+            every { repository.isLogin() } returns true
+            viewModel.navigateToPage()
+            val observer = mockk<Observer<Boolean>>(relaxed = true)
+            viewModel.isNavigateToMainActivity.observeForever(observer)
+            viewModel.viewModelScope.coroutineContext[Job]?.children?.forEach { it.join() }
+            verify { observer.onChanged(match { it }) }
+        }
+    }
+
+    @Test
+    fun navigateToPage_failure() {
+        runBlocking {
+            every { repository.isLogin() } returns false
+            viewModel.navigateToPage()
+            val observer = mockk<Observer<Boolean>>(relaxed = true)
+            viewModel.isNavigateToMainActivity.observeForever(observer)
+            viewModel.viewModelScope.coroutineContext[Job]?.children?.forEach { it.join() }
+            verify { observer.onChanged(match { !it }) }
+        }
     }
 
 }
